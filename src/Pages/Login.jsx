43be6a15e328fbe60a7cf3a7e682791d44fe2d97
CojaONIX <cojaonix@gmail.com>
2024-04-26 13:41:43 +0200

    16.14 handleLogout

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -17,6 +17,8 @@ export const Login = () => {
             setUserState({'isLogged': isLogged, 'email': email});
     }
 
+    const handleLogout = () => setUserState({});
+
     return (
         <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
             <div className="card">
@@ -27,28 +29,35 @@ export const Login = () => {
                 <p>{userStateData.isLogged ? 'logged ' + userStateData.email : 'not logged'}</p>
 
                 <div className="card-body">
-                    <form>
-                        <div className="mb-3">
-                            <label htmlFor="email">Email <span className="text-danger">*</span></label>
-                            <input onInput={e => setEmail(e.currentTarget.value)}
-                                   type="text"
-                                   id="email"
-                                   className="form-control mt-2"
-                                   autoFocus
-                            />
-                        </div>
+                    {!userStateData.isLogged
+                        ?
+                        <form>
+                            <div className="mb-3">
+                                <label htmlFor="email">Email <span className="text-danger">*</span></label>
+                                <input onInput={e => setEmail(e.currentTarget.value)}
+                                       type="text"
+                                       id="email"
+                                       className="form-control mt-2"
+                                       autoFocus
+                                />
+                            </div>
 
-                        <div className="mb-3">
-                            <label htmlFor="password">Password <span className="text-danger">*</span></label>
-                            <input onInput={e => setPassword(e.currentTarget.value)}
-                                   type="text"
-                                   id="password"
-                                   className="form-control mt-2"
-                            />
-                        </div>
+                            <div className="mb-3">
+                                <label htmlFor="password">Password <span className="text-danger">*</span></label>
+                                <input onInput={e => setPassword(e.currentTarget.value)}
+                                       type="text"
+                                       id="password"
+                                       className="form-control mt-2"
+                                />
+                            </div>
 
-                        <button onClick={handleSubmit} type="button" className="btn btn-primary form-control my-2">Login</button>
-                    </form>
+                            <button onClick={handleSubmit} type="button"
+                                    className="btn btn-primary form-control my-2">Login
+                            </button>
+                        </form>
+                        : <button onClick={handleLogout} type="button"
+                                  className="btn btn-primary form-control my-2">Logout</button>
+                    }
                 </div>
 
                 <div className="card-footer text-danger">
